6951019d50d96b2d2f59e5bea081b6f245623fe8
AnnaTau <red.morigan@gmail.com>
2017-12-20 17:49:17 +0300

    обработка страницы без туров

--- src/main/java/travel/App.java
+++ src/main/java/travel/App.java
@@ -18,6 +18,7 @@ import java.util.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.sleep;
 import static travel.From.*;
 import static travel.To.*;
 
@@ -34,13 +35,13 @@ public class App
     static LocalDate finishDate = LocalDate.parse("30.03.2018", formatter);
     static String days = "9..11";
     static From from = PETERBURG;
-    static To to = EILAT;
+    static To to = SAMUI;
 
     public static void main( String[] args )
     {
         Configuration.browser = "marionette";
-        Configuration.headless = false;
-        Configuration.timeout = 15000;
+        Configuration.headless = true;
+        Configuration.timeout = 10000;
         System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE,"true");
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\Tau\\IdeaProjects\\searching\\src\\main\resources\\chromedriver.exe");
         System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE,"/dev/null");
@@ -55,7 +56,8 @@ public class App
 
     public static void SearchTours() throws IOException {
         ArrayList<Tour> cheapestPrices = new ArrayList<>();
-        String csvFile = "C:\\Users\\Tau\\Google Диск\\leveltravel\\"+to.name()+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd_MM_yyyy__hh_mm_ss"))+".csv";
+//        String csvFile = "C:\\Users\\Tau\\Google Диск\\leveltravel\\"+to.name()+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd_MM_yyyy__hh_mm_ss"))+".csv";
+        String csvFile = "/home/user/Documents/"+to.name()+ LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd_MM_yyyy__hh_mm_ss"))+".csv";
         FileWriter writer = new FileWriter(csvFile);
         long period = ChronoUnit.DAYS.between(startDate, finishDate);
         LocalDate date = LocalDate.parse(startDate.format(formatter), formatter);
@@ -63,13 +65,24 @@ public class App
             String url = "https://level.travel/search/"+from+"-RU-to-"+to+"-departure-"+date.format(formatter)+"-for-"+days+"-nights-2-adults-0-kids-1..5-stars";
             log.info("Starting search for "+date.format(formatter));
             open(url);
-            $(".loading-block").waitWhile(visible, 60000);
-            if($(".no-results").is(visible)){
+            SelenideElement loadingBlock = $(".loading-block").waitWhile(visible, 200000);
+            log.debug("loading-block is visible? "+loadingBlock.is(visible));
+
+            //Если туров нет
+            SelenideElement emptyBlock = $(".no-results");
+            SelenideElement resultsList = $("#search_results").waitUntil(not(empty), 20000);
+            if (loadingBlock.is(not(visible)) && emptyBlock.is(not(visible))&&resultsList.is(empty)){
+                log.debug("Невидимы оба блока");
+                emptyBlock.waitUntil(visible, 5000);
+            }
+            log.debug("Emptyblock is visible? "+ emptyBlock.is(visible));
+            if(emptyBlock.is(visible)){
                 log.info("Нет туров на эту дату");
                 date = date.plusDays(1);
                 continue;
             }
-            SelenideElement resultsList = $("#search_results").should(not(empty));
+
+            //Если туры есть
             ElementsCollection hotels = resultsList.$$(".hotel_select");
             //форматируем список
             ArrayList<Tour> dayList = new ArrayList<>();
